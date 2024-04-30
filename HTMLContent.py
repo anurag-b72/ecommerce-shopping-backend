d495498fc1a9a829e8d166651fc6af0c861f9dbf
Anurag Biswal <anurag.biswal0702@gmail.com>
2024-04-30 22:12:45 +0530

    added home page

---
+++ HTMLContent.py
@@ -0,0 +1,53 @@
+html_content = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ecommerce Shopping App - Backend</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #f8f8f8;
+            }
+            .container {
+                width: 80%;
+                margin: auto;
+                text-align: center;
+                padding-top: 100px;
+            }
+            h1 {
+                color: #333;
+            }
+            p {
+                color: #666;
+                margin-top: 20px;
+            }
+            .button {
+                display: inline-block;
+                background-color: #4CAF50;
+                color: white;
+                padding: 10px 20px;
+                text-align: center;
+                text-decoration: none;
+                font-size: 16px;
+                margin-top: 20px;
+                cursor: pointer;
+                border-radius: 5px;
+            }
+            .button:hover {
+                background-color: #45a049;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Welcome to our Ecommerce Shopping App</h1>
+            <p>"Behind Every Great Shopping Experience, There's Our Backend"</p>
+            <a href="/api" class="button">Head on to Backend APIs</a>
+        </div>
+    </body>
+    </html>
+    """
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 
 from routes.AdminRoute import adminRouter
 from routes.UserRoute import userRouter
@@ -7,10 +8,15 @@ from routes.ProductRoute import productRouter
 from routes.ShoppingCartRoute import shoppingCartRouter
 from routes.OrderRoute import orderRoute
 
+from HTMLContent import html_content
+
 app = FastAPI(
     title="Ecommerce Shopping App - Backend",
     docs_url="/api",
 )
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return HTMLResponse(content=html_content)
 
 app.include_router(adminRouter)
 app.include_router(userRouter)
